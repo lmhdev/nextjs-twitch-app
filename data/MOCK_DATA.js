72848a03dd7afe7a7b21d146135648d84820a564
Minh <lm.hoang@tego.global>
2022-06-24 17:41:35 +0700

    First commit

---
+++ data/MOCK_DATA.js
@@ -0,0 +1,108 @@
+export const rec_channels = [
+  {
+    id: 1,
+    avatar: 'https://robohash.org/ipsumperspiciatisqui.png?size=50x50&set=set1',
+    username: 'erappaport0',
+    game_name: 'Bigtax',
+    rank: 11,
+  },
+  {
+    id: 2,
+    avatar: 'https://robohash.org/voluptatesvelitet.png?size=50x50&set=set1',
+    username: 'akleinlerer1',
+    game_name: 'Zathin',
+    rank: 21,
+  },
+  {
+    id: 3,
+    avatar:
+      'https://robohash.org/veritatispariaturvoluptatibus.png?size=50x50&set=set1',
+    username: 'cpetchell2',
+    game_name: 'Bitwolf',
+    rank: 2,
+  },
+  {
+    id: 4,
+    avatar: 'https://robohash.org/estquisequi.png?size=50x50&set=set1',
+    username: 'ahuburn3',
+    game_name: 'Alphazap',
+    rank: 32,
+  },
+  {
+    id: 5,
+    avatar:
+      'https://robohash.org/nihilmolestiaetemporibus.png?size=50x50&set=set1',
+    username: 'amagne4',
+    game_name: 'Latlux',
+    rank: 10,
+  },
+  {
+    id: 6,
+    avatar: 'https://robohash.org/molestiaesaepesed.png?size=50x50&set=set1',
+    username: 'nhattoe5',
+    game_name: 'Span',
+    rank: 36,
+  },
+  {
+    id: 7,
+    avatar:
+      'https://robohash.org/deseruntculpaaspernatur.png?size=50x50&set=set1',
+    username: 'jroo6',
+    game_name: 'Konklab',
+    rank: 70,
+  },
+  {
+    id: 8,
+    avatar: 'https://robohash.org/suntrerumaperiam.png?size=50x50&set=set1',
+    username: 'celan7',
+    game_name: 'It',
+    rank: 100,
+  },
+  {
+    id: 9,
+    avatar: 'https://robohash.org/omnisipsaaut.png?size=50x50&set=set1',
+    username: 'dwitz8',
+    game_name: 'Matsoft',
+    rank: 94,
+  },
+  {
+    id: 10,
+    avatar:
+      'https://robohash.org/inciduntveniamasperiores.png?size=50x50&set=set1',
+    username: 'epatterson9',
+    game_name: 'Bitchip',
+    rank: 15,
+  },
+];
+
+export const top_channels = [
+  {
+    id: 1,
+    avatar: 'https://robohash.org/ipsumperspiciatisqui.png?size=50x50&set=set1',
+    username: 'erappaport0',
+    game_name: 'Bigtax',
+    rank: 11,
+  },
+  {
+    id: 2,
+    avatar: 'https://robohash.org/voluptatesvelitet.png?size=50x50&set=set1',
+    username: 'akleinlerer1',
+    game_name: 'Zathin',
+    rank: 21,
+  },
+  {
+    id: 3,
+    avatar:
+      'https://robohash.org/veritatispariaturvoluptatibus.png?size=50x50&set=set1',
+    username: 'cpetchell2',
+    game_name: 'Bitwolf',
+    rank: 2,
+  },
+  {
+    id: 4,
+    avatar: 'https://robohash.org/estquisequi.png?size=50x50&set=set1',
+    username: 'ahuburn3',
+    game_name: 'Alphazap',
+    rank: 32,
+  },
+];
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'robohash.org',
+      'static-cdn.jtvnw.net',
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
